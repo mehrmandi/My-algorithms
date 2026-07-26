30ab7417794ff17812367573fabadb033aa99fd1
mehrmandi <134314647+mehrmandi@users.noreply.github.com>
2026-07-26 19:32:42 +0330

    add Complete Binary Tree Traversal with Array Input

---
+++ POTD/Complete Binary Tree Traversal with Array Input.py
@@ -0,0 +1,34 @@
+# Level-Based Partition and Sort - O(n log n) Time and O(n) Space
+
+def levelSort(arr):
+    n = len(arr)
+    level = 0
+    # Initialize the result list with the first level containing the root node
+    res = [[arr[0]]]
+    i = 1
+    
+    # Loop through the array to partition it into levels and sort each level
+    while i < n:
+        level += 1
+        
+        # Initialize a temporary list to store the nodes of the current level
+        q = []
+        
+        # Calculate the number of nodes at the current level
+        level_num = 2 ** level if(i + 2 ** level) < n else n - i
+        
+        # Append the nodes of the current level to the temporary list
+        for j in range(level_num):
+            q.append(arr[i + j])
+            
+        q.sort()
+        res.append(q)
+        
+        # Increment the index to move to the next level
+        i += level_num
+    
+    return res
+            
+
+arr = [7, 16, 1, 4, 13]
+print(levelSort(arr))
--- POTD/Max Sum Square Sub-Matrix of Size k.py
+++ POTD/Max Sum Square Sub-Matrix of Size k.py
@@ -12,19 +12,29 @@ def maximumSum(mat, k):
 
         # Update column sums with new row entering and old row leaving window
         for j in range(n):
+            print("i, j, colsum", i, j, colSum)
             colSum[j] += mat[i][j]
             if i >= k:
+                print("i >= k", mat[i - k][j])
                 colSum[j] -= mat[i - k][j]
 
         # Slide horizontal window of size k over colSum
         if i >= k - 1:
             windowSum = 0
+            print("i >= k", i, windowSum)
             for j in range(n):
                 windowSum += colSum[j]
+                print('j', windowSum)
                 if j >= k:
                     windowSum -= colSum[j - k]
+                    print("j >= k", windowSum)
                 if j >= k - 1:
                     res = max(res, windowSum)
+                    print("j >= k - 1", windowSum, res)
 
     return res
+
+
+mat = [[1, 2, -1, 4], [-8, -3, 4, 2], [3, 8, 10, -8], [-4, -1, 1, 7]]
+print(maximumSum(mat, 3))
         
