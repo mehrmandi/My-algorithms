879215a0a81e89ceec90e321d9e844bf9201db19
mehrmandi <134314647+mehrmandi@users.noreply.github.com>
2026-08-14 14:22:43 +0330

    edit: Subset Sum Problem

--- POTD/Subset Sum Problem.py
+++ POTD/Subset Sum Problem.py
@@ -1,51 +1,58 @@
+# Given an array arr[] of non-negative integers and a value sum, the task is to check if there is a subset of the given array whose sum is equal to the given sum.
+
+
+# Using Recursion – O(2 ^ n) Time and O(n) Space
+
 # def isSumRec(arr, n, sum):
 #     # Base Cases
 #     if sum == 0:
 #         return True
 #     if n == 0:
 #         return False
-#
+
 #     # If the last element is greater
 #     # than the sum, ignore it
 #     if arr[n - 1] > sum:
 #         return isSubsetSumRec(arr, n - 1, sum)
-#
+
 #     # Check if sum can be obtained by including
 #     # or excluding the last element
 #     return (isSubsetSumRec(arr, n - 1, sum) or
 #             isSubsetSumRec(arr, n - 1, sum - arr[n - 1]))
-#
-#
+
+
 # def isSubsetSum(arr, sum):
 #     return isSubsetSumRec(arr, len(arr), sum)
 
 
 # ----------------------------------------------------
+# Using Space Optimized DP – O(sum*n) Time and O(sum) Space
 
+def isSubsetSum(arr, sum):
+    n = len(arr)
+    prev = [False] * (sum + 1)
+    curr = [False] * (sum + 1)
 
-# def isSubsetSum(arr, sum):
-#     n = len(arr)
-#     prev = [False] * (sum + 1)
-#     curr = [False] * (sum + 1)
-#
-#     # Base case: sum 0 can always
-#     # be achieved
-#     prev[0] = True
-#
-#     # Fill the dp table in a
-#     # bottom-up manner
-#     for i in range(1, n + 1):
-#         for j in range(sum + 1):
-#             if j < arr[i - 1]:
-#                 curr[j] = prev[j]
-#             else:
-#                 curr[j] = prev[j] or prev[j - arr[i - 1]]
-#         prev = curr.copy()
-#
-#     return prev[sum]
+    # Base case: sum 0 can always
+    # be achieved
+    prev[0] = True
+
+    # Fill the dp table in a
+    # bottom-up manner
+    for i in range(1, n + 1):
+        for j in range(sum + 1):
+            if j < arr[i - 1]:
+                curr[j] = prev[j]
+            else:
+                curr[j] = prev[j] or prev[j - arr[i - 1]]
+        prev = curr.copy()
+
+    return prev[sum]
 
 
 # -----------------------------------------------------
+
+# Using Bottom-Up DP (Tabulation) - O(sum*n) Time and O(sum*n) Space
 
 # def isSubsetSum(arr, sum):
 #     n = len(arr)
@@ -76,5 +83,4 @@
 
 arr = [2, 4, 11, 10, 5]
 k = 16
-# print(isSubsetSum(arr, k))
-print(isSubsetSumRec(arr, len(arr), 16))
+print(isSubsetSum(arr, k))
---
+++ POTD/Subset Sum on Generated Sequence.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def isPossible(arr, s, x):
+    n = len(arr)
+    q = deque()
+    
+    q.append(s)
+    
+    for i in range(n):
+        
+    
+
+
+arr = [1, 2, 4, 2]
+s = 1
+x = 7
