e618ed54e7174a83d172f96814d8c05aa8b292cf
mehrmandi <134314647+mehrmandi@users.noreply.github.com>
2026-08-25 21:02:16 +0330

    add: Minimum Moves to Sort Permutation

--- POTD/Minimum Moves to Sort Permutation.py
+++ POTD/Minimum Moves to Sort Permutation.py
@@ -1,7 +1,23 @@
 def minMoves(arr):
     n = len(arr)
     
-            
+    pos_hash = {num: i for i, num in enumerate(arr)}
     
-arr = [4, 3, 1, 2]
+    LCSI = 0
+    current_chain = 0
+    
+    for i in range(1, n + 1):
+        if i == 1 or pos_hash[i] > pos_hash[i - 1]:
+            current_chain += 1
+        
+        else:
+            current_chain = 1
+    
+        LCSI = max(LCSI, current_chain)
+    
+    
+    return n - LCSI
+         
+            
+arr = [6, 2, 4, 3, 1, 5]
 print(minMoves(arr))
