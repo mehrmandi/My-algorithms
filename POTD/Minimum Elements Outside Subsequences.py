a70cd8801233fa3f595343664c6446b51c00b7d6
mehrmandi <134314647+mehrmandi@users.noreply.github.com>
2026-09-07 20:47:20 +0330

    edit: Minimum Elements Outside Subsequences

--- POTD/Minimum Elements Outside Subsequences.py
+++ POTD/Minimum Elements Outside Subsequences.py
@@ -11,28 +11,38 @@ def minCount(arr):
 
     n = len(arr)
 
-    dp = [[[0] * (n + 1) for _ in range(n + 1)]
-          for _ in range(n + 1)]
+    # Stores DP values for idx + 1.
+    next = [[0] * (n + 1) for _ in range(n + 1)]
 
+    # Stores DP values for the current index.
+    curr = [[0] * (n + 1) for _ in range(n + 1)]
+
+    # Fill the DP table in reverse order.
     for idx in range(n - 1, -1, -1):
 
         for incLast in range(-1, n):
 
             for decLast in range(-1, n):
 
-                ans = 1 + dp[idx + 1][incLast + 1][decLast + 1]
+                # Option 1: Skip the current element.
+                ans = 1 + next[incLast + 1][decLast + 1]
 
+                # Option 2: Include in increasing subsequence.
                 if incLast == -1 or arr[idx] > arr[incLast]:
                     ans = min(ans,
-                              dp[idx + 1][idx + 1][decLast + 1])
+                              next[idx + 1][decLast + 1])
 
+                # Option 3: Include in decreasing subsequence.
                 if decLast == -1 or arr[idx] < arr[decLast]:
                     ans = min(ans,
-                              dp[idx + 1][incLast + 1][idx + 1])
+                              next[incLast + 1][idx + 1])
 
-                dp[idx][incLast + 1][decLast + 1] = ans
+                curr[incLast + 1][decLast + 1] = ans
 
-    return dp[0][0][0]
+        # Move the current layer to the next layer.
+        next, curr = curr, next
+
+    return next[0][0]
 
 
 arr = [3, 1, 2, 5, 4]
