a0b3f766af0c1451c8b2d317a5d493ea483b0bda
mehrmandi <134314647+mehrmandi@users.noreply.github.com>
2026-09-21 22:44:36 +0330

    edit: Max Circular Subarray sum

--- POTD/Max Circular Subarray Sum.py
+++ POTD/Max Circular Subarray Sum.py
@@ -1,70 +1,44 @@
-# [Expected Approach] Using Kadane's Algorithm – O(n) Time and O(1) Space-------------------------------------------
+# Given a circular array arr[], find the maximum sum of any non-empty subarray. A circular array allows wrapping from the end back to the beginning.
+# Note: A subarray may wrap around the end and continue from the beginning, forming a circular segment.
 
-
-def kadane(arr):
-    max_ending = max_so_far = arr[0]
-    for x in arr[1:]:
-        max_ending = max(x, max_ending + x)
-        max_so_far = max(max_so_far, max_ending)
-    return max_so_far
-
-
-def max_circular_subarray(arr):
-    max_kadane = kadane(arr)
-    total_sum = sum(arr)
-    inverted_arr = [-x for x in arr]
-    min_subarray_sum = kadane(inverted_arr)
-    circular_max = total_sum + min_subarray_sum  # because we negated the array
-
-    # Edge case: all elements are negative
-    if circular_max == 0:
-        return max_kadane
-    
-    return max(max_kadane, circular_max)
-
-
-# arr = [10, -3, -4, 7, 6, 5, -4, -1]  # [10, -3, -4, 7, 6, 5, -4, -1]
-arr = arr = [14, -49, 25, 39, 41, -22, 30, -37, -14, -28, -43, 19, 0, 28, 43, -18, 16, 21, 42, -27, -20, -19, 29, 21, 30, 29, -25, 23, 2, -2]
-# 270
-print(max_circular_subarray(arr))
 
 
 # [Expected Approach] Using Kadane's Algorithm – O(n) Time and O(1) Space-------------------------------------------
 
-# def maxCircularSum(arr):
+def maxCircularSum(arr):
 
-#     totalSum = 0
-#     currMaxSum = 0
-#     currMinSum = 0
-#     maxSum = arr[0]
-#     minSum = arr[0]
+    totalSum = 0
+    currMaxSum = 0
+    currMinSum = 0
+    maxSum = arr[0]
+    minSum = arr[0]
 
-#     for i in range(len(arr)):
+    for i in range(len(arr)):
 
-#         # Kadane's to find maximum sum subarray
-#         currMaxSum = max(currMaxSum + arr[i], arr[i])
-#         maxSum = max(maxSum, currMaxSum)
+        # Kadane's to find maximum sum subarray
+        currMaxSum = max(currMaxSum + arr[i], arr[i])
+        maxSum = max(maxSum, currMaxSum)
 
-#         # Kadane's to find minimum sum subarray
-#         currMinSum = min(currMinSum + arr[i], arr[i])
-#         minSum = min(minSum, currMinSum)
+        # Kadane's to find minimum sum subarray
+        currMinSum = min(currMinSum + arr[i], arr[i])
+        minSum = min(minSum, currMinSum)
 
-#         # Sum of all the elements of input array
-#         totalSum += arr[i]
+        # Sum of all the elements of input array
+        totalSum += arr[i]
 
-#     normalSum = maxSum
-#     circularSum = totalSum - minSum
+    normalSum = maxSum
+    circularSum = totalSum - minSum
 
-#     # If the minimum subarray is equal to total Sum
-#     # then we just need to return normalSum
-#     if minSum == totalSum:
-#         return normalSum
+    # If the minimum subarray is equal to total Sum
+    # then we just need to return normalSum
+    if minSum == totalSum:
+        return normalSum
 
-#     return max(normalSum, circularSum)
+    return max(normalSum, circularSum)
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     arr = [8, -8, 9, -9, 10, -11, 12]
+    arr = [8, -8, 9, -9, 10, -11, 12]
 
-#     print(maxCircularSum(arr))
+    print(maxCircularSum(arr))
