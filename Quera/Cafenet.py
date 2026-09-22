d6226774a0f4314eefb48b922d57546660a5682a
mehrmandi <134314647+mehrmandi@users.noreply.github.com>
2026-09-22 16:51:26 +0330

    edit: Cafenet

--- Quera/Cafenet.py
+++ Quera/Cafenet.py
@@ -1,10 +1,17 @@
-x, y = [int(i) for i in input().split(" ")]
+import sys
+import os
 
-def cafenetCap(c, g):
+if os.path.exists("input.txt"):
+    sys.stdin = open("input.txt", "r")
+
+
+def cafenetCap(c, g, it):
     comp = "0" * c
     for i in range(g):
         flag = True
-        s, n = [int(r) for r in input().split(" ")]
+        s = int(next(it))
+        n = int(next(it))
+
         while flag and s + n <= len(comp) + 1:
             if comp[s - 1: s + n - 1] == "0" * n:
                 comp = comp[0:s - 1] + "1" * n + comp[s - 1 + n:]
@@ -13,4 +20,20 @@ def cafenetCap(c, g):
                 s += 1
         print(comp)
 
-cafenetCap(x, y)
+
+
+def main():
+    input_data = sys.stdin.buffer.read().split()
+    if not input_data:
+        return
+
+    it = iter(input_data)
+
+    x = int(next(it))
+    y = int(next(it))
+
+    cafenetCap(x, y, it)
+
+
+if __name__ == "__main__":
+    main()
